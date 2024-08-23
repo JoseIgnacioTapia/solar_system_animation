70a1feb1787decfe022a66f962e83fc473ce1560
JoseIgnacioTapia <josignaciot@gmail.com>
2024-08-23 16:52:17 -0300

    Adding materials

--- src/script.js
+++ src/script.js
@@ -8,71 +8,100 @@ const pane = new Pane();
 // initialize the scene
 const scene = new THREE.Scene();
 
+// add textureLoader
+const textureLoader = new THREE.TextureLoader();
+
+// adding textures
+const sunTexture = textureLoader.load('./textures/2k_sun.jpg');
+const mercuryTexture = textureLoader.load('./textures/2k_mercury.jpg');
+const venusTexture = textureLoader.load('./textures/2k_venus_surface.jpg');
+const earthTexture = textureLoader.load('./textures/2k_earth_daymap.jpg');
+const marsTexture = textureLoader.load('./textures/2k_mars.jpg');
+const moonTexture = textureLoader.load('./textures/2k_moon.jpg');
+
+// add materials
+const mercuryMaterial = new THREE.MeshStandardMaterial({
+  map: mercuryTexture,
+});
+const venusMaterial = new THREE.MeshStandardMaterial({
+  map: venusTexture,
+});
+const earthMaterial = new THREE.MeshStandardMaterial({
+  map: earthTexture,
+});
+const marsMaterial = new THREE.MeshStandardMaterial({
+  map: marsTexture,
+});
+const moonMaterial = new THREE.MeshStandardMaterial({
+  map: moonTexture,
+});
+
 // add stuff here
 const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
 const sunMaterial = new THREE.MeshBasicMaterial({
-  color: 0xfff700,
+  map: sunTexture,
 });
+
 const sun = new THREE.Mesh(sphereGeometry, sunMaterial);
 
 sun.scale.setScalar(5);
 scene.add(sun);
 
-const planets = [
-  {
-    name: 'Mercury',
-    radius: 0.5,
-    distance: 10,
-    speed: 0.01,
-    material: mercuryMaterial,
-    moons: [],
-  },
-  {
-    name: 'Venus',
-    radius: 0.8,
-    distance: 15,
-    speed: 0.007,
-    material: venusMaterial,
-    moons: [],
-  },
-  {
-    name: 'Earth',
-    radius: 1,
-    distance: 20,
-    speed: 0.005,
-    material: earthMaterial,
-    moons: [
-      {
-        name: 'Moon',
-        radius: 0.3,
-        distance: 3,
-        speed: 0.015,
-      },
-    ],
-  },
-  {
-    name: 'Mars',
-    radius: 0.7,
-    distance: 25,
-    speed: 0.003,
-    material: marsMaterial,
-    moons: [
-      {
-        name: 'Phobos',
-        radius: 0.1,
-        distance: 2,
-        speed: 0.02,
-      },
-      {
-        name: 'Deimos',
-        radius: 0.2,
-        distance: 3,
-        speed: 0.015,
-        color: 0xffffff,
-      },
-    ],
-  },
-];
+// const planets = [
+//   {
+//     name: 'Mercury',
+//     radius: 0.5,
+//     distance: 10,
+//     speed: 0.01,
+//     material: mercuryMaterial,
+//     moons: [],
+//   },
+//   {
+//     name: 'Venus',
+//     radius: 0.8,
+//     distance: 15,
+//     speed: 0.007,
+//     material: venusMaterial,
+//     moons: [],
+//   },
+//   {
+//     name: 'Earth',
+//     radius: 1,
+//     distance: 20,
+//     speed: 0.005,
+//     material: earthMaterial,
+//     moons: [
+//       {
+//         name: 'Moon',
+//         radius: 0.3,
+//         distance: 3,
+//         speed: 0.015,
+//       },
+//     ],
+//   },
+//   {
+//     name: 'Mars',
+//     radius: 0.7,
+//     distance: 25,
+//     speed: 0.003,
+//     material: marsMaterial,
+//     moons: [
+//       {
+//         name: 'Phobos',
+//         radius: 0.1,
+//         distance: 2,
+//         speed: 0.02,
+//       },
+//       {
+//         name: 'Deimos',
+//         radius: 0.2,
+//         distance: 3,
+//         speed: 0.015,
+//         color: 0xffffff,
+//       },
+//     ],
+//   },
+// ];
 
 // const earthMaterial = new THREE.MeshBasicMaterial({
 //   color: 'blue',
