58adc944f69a32eb28e80c93be4cbbdb52671b41
JoseIgnacioTapia <josignaciot@gmail.com>
2024-08-23 17:26:56 -0300

    Automating Mesh Generation

--- src/script.js
+++ src/script.js
@@ -47,79 +47,86 @@ const sun = new THREE.Mesh(sphereGeometry, sunMaterial);
 sun.scale.setScalar(5);
 scene.add(sun);
 
-// const planets = [
-//   {
-//     name: 'Mercury',
-//     radius: 0.5,
-//     distance: 10,
-//     speed: 0.01,
-//     material: mercuryMaterial,
-//     moons: [],
-//   },
-//   {
-//     name: 'Venus',
-//     radius: 0.8,
-//     distance: 15,
-//     speed: 0.007,
-//     material: venusMaterial,
-//     moons: [],
-//   },
-//   {
-//     name: 'Earth',
-//     radius: 1,
-//     distance: 20,
-//     speed: 0.005,
-//     material: earthMaterial,
-//     moons: [
-//       {
-//         name: 'Moon',
-//         radius: 0.3,
-//         distance: 3,
-//         speed: 0.015,
-//       },
-//     ],
-//   },
-//   {
-//     name: 'Mars',
-//     radius: 0.7,
-//     distance: 25,
-//     speed: 0.003,
-//     material: marsMaterial,
-//     moons: [
-//       {
-//         name: 'Phobos',
-//         radius: 0.1,
-//         distance: 2,
-//         speed: 0.02,
-//       },
-//       {
-//         name: 'Deimos',
-//         radius: 0.2,
-//         distance: 3,
-//         speed: 0.015,
-//         color: 0xffffff,
-//       },
-//     ],
-//   },
-// ];
+const planets = [
+  {
+    name: 'Mercury',
+    radius: 0.5,
+    distance: 10,
+    speed: 0.01,
+    material: mercuryMaterial,
+    moons: [],
+  },
+  {
+    name: 'Venus',
+    radius: 0.8,
+    distance: 15,
+    speed: 0.007,
+    material: venusMaterial,
+    moons: [],
+  },
+  {
+    name: 'Earth',
+    radius: 1,
+    distance: 20,
+    speed: 0.005,
+    material: earthMaterial,
+    moons: [
+      {
+        name: 'Moon',
+        radius: 0.3,
+        distance: 3,
+        speed: 0.015,
+      },
+    ],
+  },
+  {
+    name: 'Mars',
+    radius: 0.7,
+    distance: 25,
+    speed: 0.003,
+    material: marsMaterial,
+    moons: [
+      {
+        name: 'Phobos',
+        radius: 0.1,
+        distance: 2,
+        speed: 0.02,
+      },
+      {
+        name: 'Deimos',
+        radius: 0.2,
+        distance: 3,
+        speed: 0.015,
+        color: 0xffffff,
+      },
+    ],
+  },
+];
 
-// const earthMaterial = new THREE.MeshBasicMaterial({
-//   color: 'blue',
-// });
-// const earth = new THREE.Mesh(sphereGeometry, earthMaterial);
-// earth.position.x = 10;
+const planetMeshes = planets.map((planet) => {
+  // create the mesh
+  const planetMesh = new THREE.Mesh(sphereGeometry, planet.material);
+  // set the scale
+  planetMesh.scale.setScalar(planet.radius);
+  planetMesh.position.x = planet.distance;
+  // add it our scene
+  scene.add(planetMesh);
+  // loop through each moon and create the moon
+  planet.moons.forEach((moon) => {
+    const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
+    moonMesh.scale.setScalar(moon.radius);
+    moonMesh.position.x = moon.distance;
+    planetMesh.add(moonMesh);
+  });
 
-// scene.add(earth);
+  return planetMesh;
+});
 
-// const moonMaterial = new THREE.MeshBasicMaterial({
-//   color: 'gray',
-// });
+console.log(planetMeshes);
 
-// const moon = new THREE.Mesh(sphereGeometry, moonMaterial);
-
-// moon.scale.setScalar(0.3);
-// moon.position.x = 2;
-// earth.add(moon);
+// add lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
