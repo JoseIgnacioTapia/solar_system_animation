07b9ac048cd1b166aa1be43bda37e9326c878ff2
JoseIgnacioTapia <josignaciot@gmail.com>
2024-08-23 16:31:29 -0300

    Planet Array

--- src/script.js
+++ src/script.js
@@ -18,23 +18,79 @@ const sun = new THREE.Mesh(sphereGeometry, sunMaterial);
 sun.scale.setScalar(5);
 scene.add(sun);
 
-const earthMaterial = new THREE.MeshBasicMaterial({
-  color: 'blue',
-});
-const earth = new THREE.Mesh(sphereGeometry, earthMaterial);
-earth.position.x = 10;
+const planets = [
+  {
+    name: 'Mercury',
+    radius: 0.5,
+    distance: 10,
+    speed: 0.01,
+    material: mercuryMaterial,
+    moons: [],
+  },
+  {
+    name: 'Venus',
+    radius: 0.8,
+    distance: 15,
+    speed: 0.007,
+    material: venusMaterial,
+    moons: [],
+  },
+  {
+    name: 'Earth',
+    radius: 1,
+    distance: 20,
+    speed: 0.005,
+    material: earthMaterial,
+    moons: [
+      {
+        name: 'Moon',
+        radius: 0.3,
+        distance: 3,
+        speed: 0.015,
+      },
+    ],
+  },
+  {
+    name: 'Mars',
+    radius: 0.7,
+    distance: 25,
+    speed: 0.003,
+    material: marsMaterial,
+    moons: [
+      {
+        name: 'Phobos',
+        radius: 0.1,
+        distance: 2,
+        speed: 0.02,
+      },
+      {
+        name: 'Deimos',
+        radius: 0.2,
+        distance: 3,
+        speed: 0.015,
+        color: 0xffffff,
+      },
+    ],
+  },
+];
 
-scene.add(earth);
+// const earthMaterial = new THREE.MeshBasicMaterial({
+//   color: 'blue',
+// });
+// const earth = new THREE.Mesh(sphereGeometry, earthMaterial);
+// earth.position.x = 10;
 
-const moonMaterial = new THREE.MeshBasicMaterial({
-  color: 'gray',
-});
+// scene.add(earth);
 
-const moon = new THREE.Mesh(sphereGeometry, moonMaterial);
+// const moonMaterial = new THREE.MeshBasicMaterial({
+//   color: 'gray',
+// });
 
-moon.scale.setScalar(0.3);
-moon.position.x = 2;
-earth.add(moon);
+// const moon = new THREE.Mesh(sphereGeometry, moonMaterial);
+
+// moon.scale.setScalar(0.3);
+// moon.position.x = 2;
+// earth.add(moon);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
@@ -66,20 +122,20 @@ window.addEventListener('resize', () => {
 });
 
 // initialize a clock
-const clock = new THREE.Clock();
+// const clock = new THREE.Clock();
 
 // render loop
 const renderloop = () => {
-  const elapsedTime = clock.getElapsedTime();
+  // const elapsedTime = clock.getElapsedTime();
 
   // add animation here
-  earth.rotation.y += 0.01;
+  // earth.rotation.y += 0.01;
 
-  earth.position.x = Math.sin(elapsedTime) * 10;
-  earth.position.z = Math.cos(elapsedTime) * 10;
+  // earth.position.x = Math.sin(elapsedTime) * 10;
+  // earth.position.z = Math.cos(elapsedTime) * 10;
 
-  moon.position.x = Math.sin(elapsedTime) * 2;
-  moon.position.z = Math.cos(elapsedTime) * 2;
+  // moon.position.x = Math.sin(elapsedTime) * 2;
+  // moon.position.z = Math.cos(elapsedTime) * 2;
 
   controls.update();
   renderer.render(scene, camera);
